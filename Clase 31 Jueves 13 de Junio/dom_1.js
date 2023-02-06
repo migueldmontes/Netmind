23e2033958281c997d4229e1c85fdabd00e2be98
Miguel Angel Diaz Montes <48031654+migueldmontes@users.noreply.github.com>
2023-02-06 22:11:20 +0100

    Add files via upload

---
+++ Clase 31 Jueves 13 de Junio/dom_1.js
@@ -0,0 +1,18 @@
+// Variables
+let euros = Number (document.querySelector("text1");
+let dollarsReturn = document.querySelector(".text2");
+
+let buttonEvent = document.getElementById("form1");
+
+console.log(dollarsEvent);
+
+// Listeners
+buttonEvent.addEventListener("submit", conversorToDollars);
+
+
+// Funciones
+function conversorToDollars(euros,dollars) {
+    letDollars = euros.value = 1,12926
+    dollarsReturn.dollarsToFixed(2)
+}
+
